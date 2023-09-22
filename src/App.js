c29c299d453626638fb5e481319eddcc7afc5939
osama maree <osamamarei2001@gmail.com>
2023-09-22 20:24:38 +0300

    Add Some Configeration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import PageNotFound from "./pages/PageNotFound.jsx";
+import Home from "./pages/Home.jsx";
+import Se from "./pages/Se.jsx";
 
+const router = createBrowserRouter([
+  {
+    errorElement: <PageNotFound />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      }
+    ],
+  },
+]);
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
