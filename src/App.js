3dc38e50bb5a0945987ff81764e3ca843b81e29d
osama maree <osamamarei2001@gmail.com>
2023-09-22 23:58:19 +0300

    Edit In React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import Home from "./pages/Home.jsx";
-import Se from "./pages/Se.jsx";
+import { Box } from "@mui/material";
+import Layout from "./component/Layout.jsx";
 
 const router = createBrowserRouter([
   {
+    path: "",
+    element: <Layout />,
     errorElement: <PageNotFound />,
     children: [
       {
-        path: "/",
+        index: true,
         element: <Home />,
-      }
+      },
     ],
   },
 ]);
 function App() {
   return (
-    <>
+    <Box className="app">
       <RouterProvider router={router} />
-    </>
+    </Box>
   );
 }
 
