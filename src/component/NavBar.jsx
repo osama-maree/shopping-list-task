993c388fc4a5546078a716e4184bb056d402a889
osama maree <osamamarei2001@gmail.com>
2023-09-22 22:50:11 +0300

    Add NavBar

---
+++ src/component/NavBar.jsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+function NavBar() {
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Tooltip title="Open settings">
+            <IconButton sx={{ pr: "5px", ml: ".5%" }}>
+              <Avatar alt="logo" src="/assets/logo1.png" />
+            </IconButton>
+          </Tooltip>{" "}
+          <Typography
+            variant="h4"
+            sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
+          >
+            Shopping-Card
+          </Typography>
+          <Box sx={{ flexGrow: 0, mr: "2%" }}>
+            <Tooltip title="Open ">
+              <IconButton
+                sx={{
+                  padding: "10px",
+                }}
+              >
+                <ShoppingCartIcon
+                  sx={{
+                    color: "white",
+                    fontSize: "25px",
+                  }}
+                />
+              </IconButton>
+            </Tooltip>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default NavBar;
