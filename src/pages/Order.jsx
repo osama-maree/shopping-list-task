9852486891d8a011f186138e33d40c7ceed0b80b
osama maree <osamamarei2001@gmail.com>
2023-09-23 14:13:52 +0300

    Add Total Ammount Functionalities

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,5 +1,12 @@
-import { Container, Divider, Typography } from "@mui/material";
-import React from "react";
+import {
+  Alert,
+  Box,
+  Button,
+  Container,
+  Divider,
+  Typography,
+} from "@mui/material";
+import React, { useEffect } from "react";
 import ProductListItem from "../component/ProductListItem.jsx";
 import { useShopping } from "../context/ShoppingContext.js";
 
@@ -8,15 +15,53 @@ const Order = () => {
   const handleRemove = (id) => {
     setOrder(order.filter((ord) => ord.id !== id));
   };
+  let TotalAmmount = order.reduce((accumulator, product) => {
+    const { quantity, price } = product;
+    return accumulator + quantity * price;
+  }, 0);
+
   return (
     <Container>
-      <Typography sx={{ color: "#00acc1" }} mt={4} variant="h4" gutterBottom>
-        Orders Details
-      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+        my={3}
+      >
+        <Typography sx={{ color: "#00acc1" }} variant="h4" gutterBottom>
+          Orders Details
+        </Typography>
+        <Button
+          variant="outlined"
+          sx={{ color: "#00acc1", border: "solid 1px #00acc1" }}
+        >
+          Total Price: {TotalAmmount}
+        </Button>
+      </Box>
       <Divider />
-      {order.map((product, index) => (
-        <ProductListItem key={index} {...product} handleRemove={handleRemove} />
-      ))}
+      {order.length > 0 ? (
+        order.map((product, index) => (
+          <ProductListItem
+            key={index}
+            {...product}
+            handleRemove={handleRemove}
+          />
+        ))
+      ) : (
+        <Alert
+          severity="error"
+          sx={{
+            margin: "1rem 0px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          Please Add Item To Your Cart!&#128516;
+        </Alert>
+      )}
     </Container>
   );
 };
